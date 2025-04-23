fa97f5697d5e82c5e90b57df505e8b4e42846ca2
ToldYouNotToTell <affilacc77@gmail.com>
2025-04-24 01:25:58 +0400

    Update firebase.ts

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -3,13 +3,14 @@ import { getFirestore } from 'firebase/firestore';
 import { getAuth } from 'firebase/auth';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCvKFVCd2ylkJd3vObuLcxZQ4flCIVAqtg",
-  authDomain: "toldyounottotell.firebaseapp.com",
-  projectId: "toldyounottotell",
-  storageBucket: "toldyounottotell.firebasestorage.app",
-  messagingSenderId: "20301742943",
-  appId: "1:20301742943:web:03f96844d536f54916acea"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID
 };
+
 
 const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
